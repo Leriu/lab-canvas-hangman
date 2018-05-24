0b9887112875c8ea3fdad6bc8385772fc6308fa7
Uriel Camargo <cf_uriel@hotmail.com>
2018-05-24 18:40:40 -0500

    aun no terminado

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,56 @@
 var hangman;
 
-// function Hangman() {
+ function Hangman() {
+  this.words = ["hola", "ironhack", "jasmine", "javascript", "html", "bootstrap", "flexbox", "funciones"];
+  this.secretWord = this.getWord();
+  this.letters = [];
+  this.guessedLetter = '';
+  this.errorsLeft = 10;
+ }
 
-// }
+ Hangman.prototype.getWord = function () {
 
-// Hangman.prototype.getWord = function () {
+  var randomWord = Math.floor(Math.random() * this.words.length);
+  return this.words[randomWord];
+ };
 
-// };
+ Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (keyCode >= 65 && keyCode <= 90){
+    return true;
+  } else {
+    return false;
+  }
+ };
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+ Hangman.prototype.checkClickedLetters = function (key) {
+  return this.letters.indexOf(key.toUpperCase()) == -1;
+ };
 
-// };
+ Hangman.prototype.addCorrectLetter = function (i) {
+  this.guessedLetter += this.secretWord[i].toUpperCase();
+ }; 
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+ Hangman.prototype.addWrongLetter = function (letter) {
+  this.errorsLeft --;
+ };
 
-// };
+ Hangman.prototype.checkGameOver = function () {
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+  if (this.errorsLeft === 0) {
+    return true;
+  } else {
+    return false;
+  }
+ };
 
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+ Hangman.prototype.checkWinner = function () {
+ 
+  if (this.secretWord.length === this.guessedLetter.length) {
+    return true;
+  }else {
+    return false;
+  }
+ };
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
